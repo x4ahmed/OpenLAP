4da79dfc18aba3535fa1db12685a65189801ec9b
matinnajafi <61967440+matinnajafi@users.noreply.github.com>
2024-06-25 21:41:22 +0200

    Revert "Merge branch '17-drag-and-drop' into ISC-MATIN-CHIMA-ELHAM"
    This reverts commit 16aeb4fad97d2dcd6d19aca1d4ab8578c1384160, reversing
    changes made to 252adff09fd475639e8952b7a0ba07f4c3ca9503.

--- frontend/src/pages/Editor/IndicatorSpecificationCards/CreateIndicator/Data/DataSelection.jsx
+++ frontend/src/pages/Editor/IndicatorSpecificationCards/CreateIndicator/Data/DataSelection.jsx
@@ -343,7 +343,6 @@ export default function DataSelection({
   // Function to add a new column to the dataset
   const handleAddNewColumn = () => {
     let fieldUUID = uuidv4();
-    let fieldUUID = uuidv4();
     const newColumnData = [
       ...columnData,
       {
@@ -1004,7 +1003,6 @@ export default function DataSelection({
               onChange={(e) => setColumnName(e.target.value)}
               variant="outlined"
             />
-            />
             <Grid container>
               <Grid item xs={12}>
                 <Grid container alignItems="center">
@@ -1096,7 +1094,6 @@ export default function DataSelection({
                       error ? "Number of rows must be at least 1" : ""
                     }
                     InputProps={{
-                      inputProps: { min: 1 },
                       inputProps: { min: 1 },
                     }}
                   />
@@ -1305,6 +1302,7 @@ export default function DataSelection({
               How many rows would you like to add in the table?
             </Typography>
             <TextField
+              placeholder="Please enter a number above 0!"
               min="1"
               autoFocus
               fullWidth
@@ -1319,7 +1317,6 @@ export default function DataSelection({
               error={error}
               helperText={error ? "Number of rows must be at least 1" : ""}
               InputProps={{
-                inputProps: { min: 1 },
                 inputProps: { min: 1 },
               }}
             />
@@ -1353,20 +1350,6 @@ export default function DataSelection({
 
 const CSVUploader = ({ handlePopulateDataAndCloseModal }) => {
   const [file, setFile] = useState({ name: "" });
-  const handleDragOver = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-  };
-  const handleDragEnter = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-  };
-  const handleDrop = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-    const droppedFile = event.dataTransfer.files[0];
-    setFile(droppedFile);
-  };
 
   const handleUploadFile = () => {
     const reader = new FileReader();
@@ -1544,7 +1527,7 @@ const CSVUploader = ({ handlePopulateDataAndCloseModal }) => {
             onDragEnter={handleDragEnter}
           >
             <Link component="label" sx={{ cursor: "pointer" }}>
-              Choose a file or drag it here
+              Click here to select a file to upload
               <input
                 hidden
                 multiple
