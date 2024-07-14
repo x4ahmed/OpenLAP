8d98d511abbe0ced23df38ddeb938f87704e2eb7
Mohamed Abdelmagied <mohamedabdelmagied200@gmail.com>
2024-07-14 12:22:53 +0200

    Bug fixes to issue #6
    Fixed Color selection bug in bar charts

--- frontend/src/pages/Editor/IndicatorSpecificationCards/CreateIndicator/Visualization/Charts/BarChart.jsx
+++ frontend/src/pages/Editor/IndicatorSpecificationCards/CreateIndicator/Visualization/Charts/BarChart.jsx
@@ -94,7 +94,18 @@ export default function BarChart({
         ? "#ffffff"
         : "#000000",
     },
-    colors: [],
+    colors: [ 
+      "#33b2df",
+      "#546E7A",
+      "#d4526e",
+      "#13d8aa",
+      "#A5978B",
+      "#2b908f",
+      "#f9a3a4",
+      "#90ee7e",
+      "#f48024",
+      "#69d2e7"
+      ],
 
     title: {
       text: '',
